82bf0eb485b6594250078a87c72190545b5d1e7d
Margarita Golubeva <margaret.golubeva@mail.ru>
2023-08-04 13:37:09 +0300

    feat: solve problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function toReadable (number) {
-  
+    const string = number.toString();
+    let arr = ['', ' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine'];
+    let arrTwo = ['', '', ' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty', ' ninety']
+    let arrThree = [' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen'];
+    let result = '';
+    if (string.length === 3) {
+        switch (string[1]) {
+            case '1':
+            result = arr[string[0]] + ' hundred' + arrThree[string[2]];
+            break;
+            default:
+            result = arr[string[0]] + ' hundred' + arrTwo[string[1]] + arr[string[2]];
+            break;
+        }
+    } else if (string.length === 2) {
+        switch (string[0]) {
+            case '1':
+            result = arrThree[string[1]];
+            break;
+            default:
+            result = arrTwo[string[0]] + arr[string[1]];
+            break;
+            }
+    } else if (string[0] == 0) {
+    result = 'zero';
+    } else {
+    result = arr[string[0]];
+    }
+return result.trim();
 }
